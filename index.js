a5f2b74b3f2e90136d4654a6bf1174730fc52e16
David Ongaro <davidongaroh@gmil.com>
2022-06-16 15:13:10 +0300

    commit tree 61c2d3c6b5b49dcae3f5c4743b33ddf3511a45f8

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
 
+  const revWord= word.split('').reverse().join('')
+  
+  return word===revWord
+}
+   
 /* 
   Add your pseudocode here
+ create a word array with plit()
+ reverse &join the word array 
+  if (reversedWord === word) {
+    return true;
+    
+  }
+  else return false;
+}
 */
 
 /*
   Add written explanation of your solution here
+split the string 
+reverse the split string
+join the reversed string
+check whether the original string and reversed version are equal
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 }
 
 module.exports = isPalindrome;
